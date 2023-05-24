15bc7488d93f7599481b1b24135485d69c3b9a9b
tjupp <travisjupp@gmail.com>
2023-05-24 11:13:27 -0600

    load data with loader and useLoaderData

--- react-router-tut/src/main.jsx
+++ react-router-tut/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
-import Root from './routes/root';
+import Root, {loader as rootLoader} from './routes/root';
 import ErrorPage from './error-page';
 import Contact from './routes/contact';
 
@@ -11,6 +11,7 @@ const router = createBrowserRouter([
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    loader: rootLoader,
     children: [
       {
         path: "contacts/:contactId",
--- react-router-tut/src/routes/root.jsx
+++ react-router-tut/src/routes/root.jsx
@@ -1,47 +1,68 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useLoaderData } from "react-router-dom";
+import { getContacts } from "../contacts";
+
+export async function loader() {
+    const contacts = await getContacts();
+    return { contacts };
+}
 
 export default function Root() {
+    const {contacts} = useLoaderData();
     return (
-      <>
-        <div id="sidebar">
-          <h1>React Router Contacts</h1>
-          <div>
-            <form id="search-form" role="search">
-              <input
-                id="q"
-                aria-label="Search contacts"
-                placeholder="Search"
-                type="search"
-                name="q"
-              />
-              <div
-                id="search-spinner"
-                aria-hidden
-                hidden={true}
-              />
-              <div
-                className="sr-only"
-                aria-live="polite"
-              ></div>
-            </form>
-            <form method="post">
-              <button type="submit">New</button>
-            </form>
-          </div>
-          <nav>
+        <>
+            <div id="sidebar">
+                <h1>React Router Contacts</h1>
+                <div>
+                    <form id="search-form" role="search">
+                        <input
+                            id="q"
+                            aria-label="Search contacts"
+                            placeholder="Search"
+                            type="search"
+                            name="q"
+                        />
+                        <div
+                            id="search-spinner"
+                            aria-hidden
+                            hidden={true}
+                        />
+                        <div
+                            className="sr-only"
+                            aria-live="polite"
+                        ></div>
+                    </form>
+                    <form method="post">
+                        <button type="submit">New</button>
+                    </form>
+                </div>
+                <nav>
+                {contacts.length ? (
             <ul>
-              <li>
-                <Link to={`/contacts/1`}>Your Name</Link>
-              </li>
-              <li>
-                <Link to={`/contacts/2`}>Your Friend</Link>
-              </li>
+              {contacts.map((contact) => (
+                <li key={contact.id}>
+                  <Link to={`contacts/${contact.id}`}>
+                    {contact.first || contact.last ? (
+                      <>
+                        {contact.first} {contact.last}
+                      </>
+                    ) : (
+                      <i>No Name</i>
+                    )}{" "}
+                    {contact.favorite && <span>★</span>}
+                  </Link>
+                </li>
+              ))}
             </ul>
-          </nav>
-        </div>
-        <div id="detail">
-            <Outlet />
-        </div>
-      </>
+          ) : (
+            <p>
+              <i>No contacts</i>
+            </p>
+          )}
+                </nav>
+            </div>
+            <div id="detail">
+                <Outlet />
+            </div>
+        </>
     );
-  }
+}
