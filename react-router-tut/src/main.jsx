a72ac0301b2fe7c78d21ed683fbd9d9bde1fcb56
tjupp <travisjupp@gmail.com>
2023-05-26 08:07:04 -0600

    add / configure edit action

--- react-router-tut/src/main.jsx
+++ react-router-tut/src/main.jsx
@@ -10,7 +10,9 @@ import ErrorPage from './error-page';
 import Contact, {
   loader as contactLoader,
 } from './routes/contact';
-import EditContact from './routes/edit';
+import EditContact, {
+  action as editAction,
+} from './routes/edit';
 
 const router = createBrowserRouter([
   {
@@ -29,6 +31,7 @@ const router = createBrowserRouter([
         path: "contacts/:contactId/edit",
         element: <EditContact />,
         loader: contactLoader,
+        action: editAction,
       },
     ],
   },
