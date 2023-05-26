dc0487565450643e3fd1c4d2bf410b8bce623ec5
tjupp <travisjupp@gmail.com>
2023-05-26 07:39:38 -0600

    create / import edit component and render as a child of root

--- react-router-tut/src/main.jsx
+++ react-router-tut/src/main.jsx
@@ -10,6 +10,7 @@ import ErrorPage from './error-page';
 import Contact, {
   loader as contactLoader,
 } from './routes/contact';
+import EditContact from './routes/edit';
 
 const router = createBrowserRouter([
   {
@@ -22,6 +23,11 @@ const router = createBrowserRouter([
       {
         path: "contacts/:contactId",
         element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
         loader: contactLoader,
       },
     ],
