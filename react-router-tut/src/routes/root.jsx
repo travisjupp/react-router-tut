2e863c3df6be5442ce45aa8687e5f7739f0eb8b2
tjupp <travisjupp@gmail.com>
2023-06-01 08:34:53 -0600

    search form GET submission

--- react-router-tut/src/routes/root.jsx
+++ react-router-tut/src/routes/root.jsx
@@ -6,6 +6,7 @@ import {
     redirect,
     NavLink,
     useNavigation,
+    useSearchParams,
 } from "react-router-dom";
 import { getContacts, createContact } from "../contacts";
 
@@ -14,8 +15,10 @@ export async function action() {
     return redirect(`/contacts/${contact.id}/edit`);
 }
 
-export async function loader() {
-    const contacts = await getContacts();
+export async function loader({request}) {
+    const url = new URL(request.url);
+    const q = url.searchParams.get("q");
+    const contacts = await getContacts(q);
     return { contacts };
 }
 
@@ -28,7 +31,7 @@ export default function Root() {
             <div id="sidebar">
                 <h1>React Router Contacts</h1>
                 <div>
-                    <form id="search-form" role="search">
+                    <Form id="search-form" role="search">
                         <input
                             id="q"
                             aria-label="Search contacts"
@@ -45,7 +48,7 @@ export default function Root() {
                             className="sr-only"
                             aria-live="polite"
                         ></div>
-                    </form>
+                    </Form>
                     <Form method="post">
                         <button type="submit">New</button>
                     </Form>
