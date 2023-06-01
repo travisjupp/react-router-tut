86906f7af474a0c2643556abd24ff4d00766afde
tjupp <travisjupp@gmail.com>
2023-06-01 08:06:11 -0600

    cancel button click handler calls useNavigate

--- react-router-tut/src/routes/edit.jsx
+++ react-router-tut/src/routes/edit.jsx
@@ -2,6 +2,7 @@ import {
     Form,
     useLoaderData,
     redirect,
+    useNavigate,
 } from "react-router-dom";
 import { updateContact } from "../contacts";
 
@@ -10,10 +11,11 @@ export async function action({ request, params }) {
     const updates = Object.fromEntries(formData);
     await updateContact(params.contactId, updates);
     return redirect(`/contacts/${params.contactId}`);
-  }
+}
 
 export default function EditContact() {
     const { contact } = useLoaderData();
+    const navigate = useNavigate();
 
     return (
         <Form method="post" id="contact-form">
@@ -63,7 +65,11 @@ export default function EditContact() {
             </label>
             <p>
                 <button type="submit">Save</button>
-                <button type="button">Cancel</button>
+                <button
+                    type="button"
+                    onClick={() => {
+                        navigate(-1);
+                    }}>Cancel</button>
             </p>
         </Form>
     );
