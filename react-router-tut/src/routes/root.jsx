d5c68da82ddfd7fd7e61fcd5269585e323fa4c68
tjupp <travisjupp@gmail.com>
2023-06-01 14:28:12 -0600

    sync URL and form state
    form field value removed when no longer needed ie clicking backand form field value persists after manual page refresh

--- react-router-tut/src/routes/root.jsx
+++ react-router-tut/src/routes/root.jsx
@@ -9,6 +9,7 @@ import {
     useSearchParams,
 } from "react-router-dom";
 import { getContacts, createContact } from "../contacts";
+import { useEffect } from "react";
 
 export async function action() {
     const contact = await createContact();
@@ -19,12 +20,16 @@ export async function loader({request}) {
     const url = new URL(request.url);
     const q = url.searchParams.get("q");
     const contacts = await getContacts(q);
-    return { contacts };
+    return { contacts, q };
 }
 
 export default function Root() {
-    const { contacts } = useLoaderData();
+    const { contacts, q } = useLoaderData();
     const navigation = useNavigation();
+
+    useEffect(() => {
+        document.getElementById("q").value = q;
+    }, [q])
 
     return (
         <>
@@ -38,6 +43,7 @@ export default function Root() {
                             placeholder="Search"
                             type="search"
                             name="q"
+                            defaultValue={q}
                         />
                         <div
                             id="search-spinner"
